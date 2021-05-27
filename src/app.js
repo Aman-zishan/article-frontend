41af881ab997d59e7a159d80942fa5edf627ef09
Zishan <amanzishan.az@gmail.com>
2021-05-27 20:11:55 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -3,29 +3,30 @@ import { Container,AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import articles from './images/articles.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
     return(
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-            <img src={articles} alt="articles" height="120"/>
-            <Typography variant="h2" align="center">Articles</Typography>
-            
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
-                        </Grid>
-                        <Grid item xs={12} sm={7}>
-                            <Form />
-                        </Grid>
-
-                    </Grid>
-                </Container>
-            </Grow>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h3" align="center">Articles</Typography>
+        <img className={classes.image} src={articles} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
         </Container>
+      </Grow>
+    </Container>
+        
     )
 }
 
